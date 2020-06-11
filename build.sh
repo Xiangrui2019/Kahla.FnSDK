e6f8ad04ba6425b1640b5c51b92e4501a1880530
Anduin Xue <anduin@aiursoft.com>
2020-06-11 17:08:53 +0800

    Simplify some code.

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
-rm ./node_modules -rf
 rm ./dist -rf
-rm ./package-lock.json
-npm i
 npm run build
 npm run build --production
 start ./example.html
--- src/Kahla.ts
+++ src/Kahla.ts
@@ -1,14 +1,18 @@
 import { AuthService } from './Services/AuthService';
 import { ApiService } from './Services/ApiService';
 import { HttpClient } from '@angular/common/http';
-
+import { Injector } from '@angular/core';
 class Kahla {
     constructor() {
 
     }
 
     main(): void {
-        var auth = new AuthService(new ApiService(new HttpClient(null)));
+        var injector = Injector.create([
+            { provide: HttpClient, useClass: HttpClient, deps: [] }
+        ]);
+        var http = injector.get(HttpClient);
+        var auth = new AuthService(new ApiService(http));
         auth.Me().subscribe(result => {
             console.log(result.message);
         });
